153edd6bbb3ade58a763bcd9c181d0bea6b56697
yorishori <stuffandeffects@gmail.com>
2024-08-27 00:04:22 +0200

    Added favicon that doesn't work :(

--- bin/nyanRouting.js
+++ bin/nyanRouting.js
@@ -14,8 +14,9 @@ const files = {
 		{vpath:"/style.css", path:"static/css/style.css", type:"text/css"},
 		{vpath:"/index.js", path:"static/js/index.js", type:"text/css"},
 		// JS
-		{vpath:"/jquery.js", path:"lib/jquery-3.7.1.slim.min.js", type:"text/js"}
-
+		{vpath:"/jquery.js", path:"lib/jquery-3.7.1.slim.min.js", type:"text/js"},
+		// Assets
+		{vpath:"/favicon.png", path:"static/assets/favicon.png", type:"image/png"}
 	],
 	err:{
 		db:{status:503, path:"static/html/err/dberr.html"},
@@ -45,13 +46,13 @@ function nyanReq(req){
 				//routePost(req);break;
 			default:
 				res.head.status = 501;
-				res.val = `HTTP Method "${req.method}" not implemeted`;
+				res.val = `Something went wrong with resquested method :(`;
 				console.log(`Client requested unimplemented method: "${req.method}"`);
 				break;
 		}
 	}catch(err){
 		res.head.status = 404;
-		res.val = `"Something went wrong in routing HTTP request: ${err}`;
+		res.val = `Something went wrong with routing your HTTP request :(`;
 		console.log(`Internal ERROR [nyanRouting]: ${err}`);
 	}finally{	
 		return res;
@@ -69,7 +70,7 @@ function nyanGet(res, vPath){
 		if (e.vpath===vPath){
 			if(e.type==="text/html" && !nyaDB.nyActive()){
 				nyanGetErr(res, files.err.db);
-				console.log(`Internal ERROR [nyanRouting]: Database unavailble. Client requested "${e.vPath}" but will be delivered error page instead.`);
+				console.log(`Internal ERROR [nyanRouting]: Database unavailble. Client requested "${vPath}" but will be delivered error page instead.`);
 			}else{	
 				getFile(res, e.path, e.type);
 			}
